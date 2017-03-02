01fc8ab614ae0002b3971fc2e3c09cefee593369
Thomas Getzoyan <tgetzoya@gmail.com>
2017-03-01 16:32:08 -0800

    Problem 21 works.

--- problems/pe21.py
+++ problems/pe21.py
@@ -1,13 +1,33 @@
 from utils.factors import get_factors
+from utils.primes import is_prime
 
+
+# Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+# If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+#
+# For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+#
+# Evaluate the sum of all the amicable numbers under 10000.
 
 def run():
     map = {}
-    sum = 0
-    for idx in range(1, 100):
-        map[idx] = sum(get_factors(idx, True))
-    for key, value in map.items():
-        if (value in map) and (map[value] == key):
-            sum += key + value
-            del map[key]
-            del map[value]
+    items = set()
+    for idx in range(4, 10000):
+        if not is_prime(idx):
+            map[idx] = d(idx)
+
+    for idx in map.keys():
+        if map[idx] in map:
+            right_side = map[map[idx]]
+        else:
+            right_side = -1
+
+        if right_side == idx and not idx == map[idx]:
+            items.add(idx)
+            items.add(map[idx])
+
+    return sum(items)
+
+
+def d(n):
+    return sum(get_factors(n)) + 1
