c459dc0f853c27006db6865be731ad53ee2cd778
Thomas Getzoyan <tgetzoya@gmail.com>
2017-03-03 15:14:44 -0800

    29 Works

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import timeit
 
-from problems import pe25
+from problems import pe29
 
 
 def main():
     start = timeit.default_timer()
-    print(pe25.run())
+    print(pe29.run())
     end = timeit.default_timer()
     print("{} Seconds".format(format(end - start, '.4g')))
 
